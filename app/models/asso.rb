4bbdd7779162aa0b9e241a4f4dcb79d84313ac17
mad221 <maximepouzol@gmail.com>
2019-03-12 19:32:07 +0100

    ajout card

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,5 +1,5 @@
 class Asso < ApplicationRecord
-validates :name, presence: true, uniqueness: true
+validates :name, presence: true, #uniqueness: true
 validates :description, presence: true, length: {in: 20..2000}
 belongs_to :owner, class_name: "User"
 
