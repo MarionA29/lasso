5df6271bab37918bd07fdf620363f55841b17c2a
mad221 <maximepouzol@gmail.com>
2019-03-12 17:27:53 +0100

    ajout active storage sur model Asso

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -2,4 +2,7 @@ class Asso < ApplicationRecord
 validates :name, presence: true, uniqueness: true
 validates :description, presence: true, length: {in: 20..2000}
 belongs_to :owner, class_name: "User"
+
+
+has_many_attached :picture
 end
