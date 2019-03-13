2396643344ba873336cb359c2712b70afc342347
mad221 <maximepouzol@gmail.com>
2019-03-13 15:26:29 +0100

    ajout create dans controller assos

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,9 +1,9 @@
 class Asso < ApplicationRecord
 
-validates :name, presence: true #uniqueness: true
+  validates :name, presence: true #uniqueness: true
 
-validates :description, presence: true, length: {in: 20..2000}
-belongs_to :owner, class_name: "User"
+  validates :description, presence: true, length: {in: 20..2000}
+  belongs_to :owner, class_name: "User"
 
-has_many_attached :picture
+  has_many_attached :picture
 end
