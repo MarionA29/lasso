acf9b8d461514489a49e5021223336363335504f
MarionA29 <marionaudren@live.fr>
2019-03-13 20:45:26 +0100

    minime changes

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -3,11 +3,12 @@ validates :name, presence: true
 validates :description, presence: true, length: {in: 20..2000}
 belongs_to :owner, class_name: "User"
 has_one :subscription
+has_many_attached :picture
+after_create :ask_validation
 
-  validates :name, presence: true #uniqueness: true
+def ask_validation
+  UserMailer.asso_creation_email(self).deliver_now
+  AdminMailer.validation_email(self).deliver_now
+end
 
-  validates :description, presence: true, length: {in: 20..2000}
-  belongs_to :owner, class_name: "User"
-
-  has_many_attached :picture
 end
