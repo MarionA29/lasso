2a2702e02aeddae0675c9aecd3377aa843dfcfc2
mad221 <maximepouzol@gmail.com>
2019-03-13 11:56:31 +0100

    composant creer nav, footer, card

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,8 +1,7 @@
 class Asso < ApplicationRecord
-validates :name, presence: true, #uniqueness: true
+validates :name, presence: true #uniqueness: true
 validates :description, presence: true, length: {in: 20..2000}
 belongs_to :owner, class_name: "User"
-
 
 has_many_attached :picture
 end
