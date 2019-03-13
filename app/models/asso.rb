97b482e5ccf002acf1839ea1251811f2f78a2cd4
MarionA29 <marionaudren@live.fr>
2019-03-13 16:25:18 +0100

    save conflict

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,11 +1,8 @@
 class Asso < ApplicationRecord
-<<<<<<< HEAD
 validates :name, presence: true
 validates :description, presence: true, length: {in: 20..2000}
 belongs_to :owner, class_name: "User"
 has_one :subscription
-=======
->>>>>>> 53ee69c8da53aa8b77f5e4cb9c445fc9401a14e0
 
   validates :name, presence: true #uniqueness: true
 
