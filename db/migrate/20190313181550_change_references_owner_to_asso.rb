943ab9cbbac04f6ff7011b4a6362e9b7bb736752
totaotata <totaotata@protonmail.com>
2019-03-13 19:38:31 +0100

    Migration change owner to asso in subscription table 2

--- db/migrate/20190313181550_change_references_owner_to_asso.rb
+++ db/migrate/20190313181550_change_references_owner_to_asso.rb
@@ -2,12 +2,7 @@ class ChangeReferencesOwnerToAsso < ActiveRecord::Migration[5.2]
   def up
   change_table :subscriptions do |t|
     t.references :asso, index: true
-    end
-  end
-
-  def down
-  change_table :subscriptions do |t|
-    t.remove_reference(:owner, index: true)
+    t.remove :owner, index: true
     end
   end
 end
