283dd61ff3688a8017dcc94a3588cf1799f15c2b
mad221 <maximepouzol@gmail.com>
2019-03-14 10:58:25 +0100

    ajout de plusieurs images

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -20,6 +20,12 @@ class AssosController < ApplicationController
 
       if @asso.save # essaie de sauvegarder en base
         # si ça marche, il redirige vers la page d'index du site
+        @asso = Asso.last
+        @asso.picture1.attach(params[:picture1])
+        @asso.picture2.attach(params[:picture2])
+        @asso.picture3.attach(params[:picture3])
+        @asso.picture4.attach(params[:picture4])
+        @asso.picture5.attach(params[:picture5])
         flash[:success] = "Ton associaiton à bien été créée !"
 
         redirect_to root_path
