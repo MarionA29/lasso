534a1df4bb031d44e8b561d77d7edbbbcf77bed2
MarionA29 <marionaudren@live.fr>
2019-03-15 13:40:45 +0100

    after conflicts

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -2,9 +2,9 @@ class Asso < ApplicationRecord
 validates :name, presence: true
 validates :description, presence: true, length: {in: 20..2000}
 validates :name, presence: true #uniqueness: true
-
 belongs_to :owner, class_name: "User"
 has_one :subscription
+
 
 has_one_attached :picture1
 has_one_attached :picture2
@@ -13,4 +13,13 @@ has_one_attached :picture4
 has_one_attached :picture5
 geocoded_by :address
 after_validation :geocode
+
+after_create :ask_validation
+
+
+def ask_validation
+  UserMailer.asso_creation_email(self).deliver_now
+  AdminMailer.validation_email(self).deliver_now
+end
+
 end
