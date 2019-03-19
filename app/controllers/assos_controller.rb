71e1676a1d987029326d949ccc13477451b87f7e
mad221 <maximepouzol@gmail.com>
2019-03-19 16:47:13 +0100

    correction controller  asso show

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -35,7 +35,11 @@ class AssosController < ApplicationController
     end
 
     def show
+      if params[:id].to_i != current_user.id
       @asso= Asso.find(params[:id])
+    else
+      @asso= Asso.where(owner_id: current_user.id).first
+    end
 
     end
 
