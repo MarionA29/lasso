9314024be73f71ed1e11f65ed8ea19880ee81e31
totaotata <totaotata@protonmail.com>
2019-03-21 23:08:52 +0100

    Ajout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'news/index'
-  get 'news/show'
-  get 'news/new'
-  get 'news/create'
-  get 'news/edit'
-  get 'news/update'
-  get 'news/destroy'
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
   root 'assos#index'
   devise_for :users
   resources :assos
+  resources :news
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :subscriptions
   resources :teams, only: [:index]
