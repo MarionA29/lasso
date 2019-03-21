11e7cbe01e115f588b5c4c6906eaf0abc81a0651
totaotata <totaotata@protonmail.com>
2019-03-21 23:31:49 +0100

    Début de controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,14 +1,32 @@
 class NewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+    @news = New.all
   end
 
   def show
+    @new = New.find(params[:id])
   end
 
   def new
+    @new = New.new
   end
 
   def create
+    @new = New.create('title' => params[:title],
+      'content' => params[:content],
+      'date' => params[:date],
+      'asso_id' => @asso.id)
+
+      if @new.save
+        #Attach picture to do
+        flash[:success] = "Ta news a bien été créée !"
+        redirect_to root_path
+      else
+        flash[:error] = "Ta news n'a pas été créée !"
+        # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+        render 'new'
+      end
   end
 
   def edit
