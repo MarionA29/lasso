57072dd712ced7ce1920503ab55239704acc5453
totaotata <totaotata@protonmail.com>
2019-03-22 18:51:07 +0100

    Mailer répraré

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,9 +3,9 @@ class Subscription < ApplicationRecord
 end
 
 
-after_create :subscription_email
-
-def subscription_email
-  UserMailer.payment_complete_email(self).deliver_now
-  AdminMailer.new_subscription_email(self).deliver_now
-end
+# after_create :subscription_email
+#
+# def subscription_email
+#   UserMailer.payment_complete_email(self).deliver_now
+#   AdminMailer.new_subscription_email(self).deliver_now
+# end
