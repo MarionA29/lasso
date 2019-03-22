1427375a4f6f3e1f3155e07e46eb071d9eebb2c4
mad221 <maximepouzol@gmail.com>
2019-03-22 11:38:19 +0100

    coorrection meethode mvc dans view

--- app/controllers/privates_messages_controller.rb
+++ app/controllers/privates_messages_controller.rb
@@ -10,14 +10,17 @@ class PrivatesMessagesController < ApplicationController
 
   def create
     @message = PrivateMessage.new
+    #ici le params asso_id ne renvoi pas le meme argument en fonction du lieu ou on se situe sur le site
     if PrivateMessage.where(sender_id: current_user.id, recipient_id:  params[:asso_id]).first != nil
-
+      # si params[:asso_id] est directement l'id du recipient alors rende stock la conversation
       rende = PrivateMessage.where(sender_id: current_user.id, recipient_id:  params[:asso_id]).first.recipient_id
     else
+      # si params[:assso_id] n'est pas le recipient, mais le numero de l'asso, alors je dois chercher le owner_id pour obtenir  le recipient_id
+      # de la je stock la conversation entre ces deux utilisateurs
       rende = PrivateMessage.where(sender_id: current_user.id, recipient_id: Asso.find(params[:asso_id]).owner_id).first.recipient_id
     end
 
-    
+
 
     @message = PrivateMessage.create(sender_id: current_user.id ,
       'content' => params[:content],
@@ -26,6 +29,7 @@ class PrivatesMessagesController < ApplicationController
       if @message.save # essaie de sauvegarder en base
         # si ça marche, il redirige vers la page d'index du site
         flash[:success] = "Ton message à bien été envoyé !"
+        #le rende rende contient le recipient_id de la conversation
         redirect_to asso_privates_message_privates_message_path(params[:asso_id] , rende , current_user.id)
       else
         # sinon, il render la view new (qui est celle sur laquelle on est déjà)
