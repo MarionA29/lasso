8acc8982a522b93189212b62281eb68a3ddd3266
mad221 <maximepouzol@gmail.com>
2019-03-22 14:20:22 +0100

    suppr espace

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -39,7 +39,7 @@ class AssosController < ApplicationController
     end
 
     def show
-      if params[:id].to_i != current_user.id
+      if params[:id] != current_user
       @asso= Asso.find(params[:id])
     else
       @asso= Asso.where(owner_id: current_user.id).first
